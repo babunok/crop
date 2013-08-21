cd9ae7db0574649ec3e07f57325aafde63db1934
babunok <babunok@yahoo.com>
2013-08-21 06:49:40 +0400

    one

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to crop
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/crop/core_test.clj
@@ -0,0 +1,7 @@
+(ns crop.core-test
+  (:use clojure.test
+        crop.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
